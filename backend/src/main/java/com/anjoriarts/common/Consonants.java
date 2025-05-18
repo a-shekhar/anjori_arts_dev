7a7d7075d32bce3063bc9eae887a7bcfe16c8fc4
a-shekhar <rajadityashekhar@outlook.com>
2025-05-18 18:48:15 +0530

    0.6 - Visitors count issue fix

--- backend/src/main/java/com/anjoriarts/common/Consonants.java
+++ backend/src/main/java/com/anjoriarts/common/Consonants.java
@@ -1,21 +1,30 @@
 package com.anjoriarts.common;
 
 public class Consonants {
+    // company details
     public static final String COMPANY_NAME = "Anjori Arts";
     public static final String COMPANY_EMAIL_1 = "anjoriarts@gmail.com";
     public static final String COMPANY_EMAIL_2 = "anjoriarts@outlook.com";
     public static final String ZONE_ID = "Asia/Kolkata";
+    // messages
     public static final int EXPIRATION_TIME_IN_MINUTES = 10;
     public static final int RESEND_TIME_IN_MINUTES = 1;
     public static final String INTERNAL_SERVER_ERROR = "Internal Server Error..";
+    // roles
+    public static final String USER_ROLE = "ROLE_USER";
+    public static final String ADMIN_ROLE = "ROLE_ADMIN";
+
     // cloudinary path
     public static final String ARTWORKS_PATH = "Anjori/arts/%s/artworks/%s"; //first s is env, 2nd s is slug
     public static final String CUSTOM_ORDERS_PATH = "Anjori/arts/%s/custom-orders/%s"; //first s is env, 2nd s is custom order id
     public static final String USER_PROFILE_IMAGES_PATH = "Anjori/arts/%s/users/%s"; //first s is env, 2nd s is user id
 
+    // redis keys
+    public static final String REDIS_VISITORS_KEY =  "anjori-%s:visitors";
+    public static final String REDIS_ACTIVE_USERS_KEY = "anjori-%s:active-users";
+    public static final String REDIS_UNIQUE_VISITORS_KEY = "anjori-%s:unique-visitors";
 
-    public static final String USER_ROLE = "ROLE_USER";
-    public static final String ADMIN_ROLE = "ROLE_ADMIN";
+    // order
     public static final String ORDER_INITIAL_STATE = "Pending";
 
 }
--- backend/src/main/java/com/anjoriarts/service/AppStatsServiceImpl.java
+++ backend/src/main/java/com/anjoriarts/service/AppStatsServiceImpl.java
@@ -1,5 +1,6 @@
 package com.anjoriarts.service;
 
+import com.anjoriarts.common.Consonants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,33 +17,31 @@ public class AppStatsServiceImpl implements AppStatsService{
     @Value("${spring.application.env}") // fallback to "prod"
     private String env;
 
-    private final String anjori = "anjori";
-    private final String visitorsKey =  anjori + "-" + env + ":visitors";
-    private final String activeUsersKey = anjori + "-" + env + ":active-users";
-    private final String uniqueVisitorsKey = anjori + "-" + env + ":unique-visitors";
-
-
     public AppStatsServiceImpl(RedisTemplate<String, String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     @Override
     public void trackUniqueVisitor(String ip) {
+        String uniqueVisitorsKey = String.format(Consonants.REDIS_UNIQUE_VISITORS_KEY, env);
         redisTemplate.opsForSet().add(uniqueVisitorsKey, ip); // Add only if not already present
     }
 
     @Override
     public long getUniqueVisitorCount() {
+        String uniqueVisitorsKey = String.format(Consonants.REDIS_UNIQUE_VISITORS_KEY, env);
         return  redisTemplate.opsForSet().size(uniqueVisitorsKey);
     }
 
     @Override
     public void trackActiveUser() {
+        String activeUsersKey = String.format(Consonants.REDIS_ACTIVE_USERS_KEY, env);
         redisTemplate.opsForValue().increment(activeUsersKey); // just increase the count
     }
 
     @Override
     public long getActiveUsersCount(){
+        String activeUsersKey = String.format(Consonants.REDIS_ACTIVE_USERS_KEY, env);
         String value = redisTemplate.opsForValue().get(activeUsersKey);
         return  (value != null) ? Long.parseLong(value) : 0;
     }
@@ -55,11 +54,13 @@ public class AppStatsServiceImpl implements AppStatsService{
 
     @Override
     public void trackVisitorCount(){
+        String visitorsKey = String.format(Consonants.REDIS_VISITORS_KEY, env);
         redisTemplate.opsForValue().increment(visitorsKey); // just increase the count
     }
 
     @Override
     public long getVisitorsCount(){
+        String visitorsKey = String.format(Consonants.REDIS_VISITORS_KEY, env);
         String value = redisTemplate.opsForValue().get(visitorsKey);
         return  (value != null) ? Long.parseLong(value) : 0;
     }
